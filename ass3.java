009e48cb7d03d6039505b9ff5ce6f6f08beebd56
Latesh08 <88035170+Latesh08@users.noreply.github.com>
2022-05-31 15:50:21 +0530

    Add files via upload

---
+++ ass3.java
@@ -0,0 +1,19 @@
+package metaoercent;
+import java.util.Arrays;
+import java.util.Collections;
+public class ass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        
+	        Integer[] num = {2, 4, 10, 12, 4, 100, 99, 4, 3, 2, 99, 0};
+	        
+	        int max = Collections.max(Arrays.asList(num));
+	 
+	        
+	       
+	System.out.println("max number" +max);
+	    
+	}
+
+}
